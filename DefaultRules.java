c9f1ca34f8296470878b3177c4603231e45d044c
Moxenial <47155699+Moxenial@users.noreply.github.com>
2019-05-09 00:38:05 -0500

    Delete DefaultRules.java

--- DefaultRules.java
+++
@@ -1,7 +0,0 @@
-
-public interface DefaultRules {
-	
-	public static final int DECKSIZE = 108; //maybe remove final modifier later?
-	public static final int STARTINGHAND = 7;
-	// public void winCondition();
-}
